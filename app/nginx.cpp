da76f97e461f20bc38fb17807ad5ddcc48290800
awen <2693344198@qq.com>
2023-02-20 18:28:23 +0800

    syn

--- app/nginx.cpp
+++ app/nginx.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "ngx_signal.h"
 using namespace std;
+
+class test{
+public:
+    const int data_com =20;
+    static int data_static;
+};
+
+int test::data_static = 10;
+
+
 int main(){
-    cout<<"wen"<<endl;
+    int i = 10;
+    int &&j = i++;
+    int &k = j;
+//    int &&u = j;
+    k = 100;
+    cout<<j<<endl;
     return 0;
 }
