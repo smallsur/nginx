c06efd7510fd1baf2680f04b2084701e973f57d0
awen <2693344198@qq.com>
2023-02-21 20:59:59 +0800

    syn

--- app/nginx.cpp
+++ app/nginx.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <memory>
 #include "ngx_signal.h"
 using namespace std;
 
 class test{
 public:
-    const int data_com =20;
-    static int data_static;
+    test(){
+        cout<<"create"<<endl;
+    };
+    ~test(){
+        cout<<"free"<<endl;
+    }
 };
-
-int test::data_static = 10;
 
 
 int main(){
-    int i = 10;
-    int &&j = i++;
-    int &k = j;
-//    int &&u = j;
-    k = 100;
-    cout<<j<<endl;
+    shared_ptr<int> p = make_shared<int>(100);
+    shared_ptr<int> p1 = p;
+    cout<<p.use_count()<<endl;
+    int *q = p1.get();
+    cout<<p.use_count()<<endl;
+//    shared_ptr<int> p2(q);
+    p1 = nullptr;
+    cout<<p.use_count()<<endl;
     return 0;
 }
